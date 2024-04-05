23dd26e9f7e9edb2f09559f22faaacf0b3ffa142
xlk <1148038404@qq.com>
2024-04-05 16:04:32 +0800

    feat: 改写demo1成Promise模式

--- promise/index.js
+++ promise/index.js
@@ -1,40 +1,24 @@
 // 给不同的人发消息，得到成功和失败的结果
 
-function sendMessage(name, resolve, reject) {
-  console.log(`沙拉嘿呦， 只对你说，${name}`);
+function sendMessage(name) {
+  return new Promise((resolve, reject) => {
+    console.log(`沙拉嘿呦， 只对你说，${name}`);
 
-  if (Math.random() < 0.1) {
-    resolve("我也喜欢你~~~");
-  } else {
-    reject("对不起，你是个好人");
-  }
+    if (Math.random() < 0.1) {
+      resolve("我也喜欢你~~~");
+    } else {
+      reject("对不起，你是个好人");
+    }
+  });
 }
 
-sendMessage(
-  "刘亦菲",
-  (replay) => {
-    console.log(replay);
+console.log(sendMessage);
+
+sendMessage("刘亦菲").then(
+  (res) => {
+    console.log(res);
   },
-  (replay) => {
-    console.log(replay);
-    sendMessage(
-      "迪丽热巴",
-      (replay) => {
-        console.log(replay);
-      },
-      (replay) => {
-        console.log(replay);
-        sendMessage(
-          "古力娜扎",
-          (replay) => {
-            console.log(replay);
-          },
-          (replay) => {
-            console.log(replay);
-            console.log("放弃");
-          }
-        );
-      }
-    );
+  (reason) => {
+    console.log(reason);
   }
 );
