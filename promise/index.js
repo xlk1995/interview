39fcbde16592992d5f24a3806c5493fc288f8544
xlk <1148038404@qq.com>
2024-04-05 15:59:34 +0800

    feat: promise的规范

--- promise/index.js
+++ promise/index.js
@@ -1,1 +1,40 @@
-console.log("init");
+// 给不同的人发消息，得到成功和失败的结果
+
+function sendMessage(name, resolve, reject) {
+  console.log(`沙拉嘿呦， 只对你说，${name}`);
+
+  if (Math.random() < 0.1) {
+    resolve("我也喜欢你~~~");
+  } else {
+    reject("对不起，你是个好人");
+  }
+}
+
+sendMessage(
+  "刘亦菲",
+  (replay) => {
+    console.log(replay);
+  },
+  (replay) => {
+    console.log(replay);
+    sendMessage(
+      "迪丽热巴",
+      (replay) => {
+        console.log(replay);
+      },
+      (replay) => {
+        console.log(replay);
+        sendMessage(
+          "古力娜扎",
+          (replay) => {
+            console.log(replay);
+          },
+          (replay) => {
+            console.log(replay);
+            console.log("放弃");
+          }
+        );
+      }
+    );
+  }
+);
---
+++ promise/readme.md
@@ -0,0 +1,5 @@
+## Promise 规范
+
+Promise 是一套处理异步场景的规范，有效的避免回调地狱
+
+Promise A+
